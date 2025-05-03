16dd72d67cfbda7414ed858bebf224fb180403b0
Nicky Jordan <spiderug400@googlemail.com>
2025-05-03 19:56:05 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HealthTrackerApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HealthTrackerApp />
+  </React.StrictMode>
+);
